7278669e200d5da9a6a180b61497f02241bfe0f6
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-10-05 19:01:36 +0900

    Checking in changes prior to tagging of version 0.03.  Changelog diff is:
    diff --git a/Changes b/Changes
    index 1f802e6..e5939f9 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,10 @@
     Revision history for Perl extension AnyEvent::MPRPC
    
    +0.03
    +
    +    - oops. $msgid should be IV.
    +      Perl type system is ...
    +
     0.02
         - oops

--- lib/AnyEvent/MPRPC.pm
+++ lib/AnyEvent/MPRPC.pm
@@ -2,7 +2,7 @@ package AnyEvent::MPRPC;
 
 use strict;
 use warnings;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 use AnyEvent::MPRPC::Server;
 use AnyEvent::MPRPC::Client;
 use base 'Exporter';
