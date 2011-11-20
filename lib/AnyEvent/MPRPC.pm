32771209e820846ee6bfa0f3d6a66aa4f8c5c2b4
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-11-21 07:31:15 +0900

    modernized package

--- lib/AnyEvent/MPRPC.pm
+++ lib/AnyEvent/MPRPC.pm
@@ -2,7 +2,7 @@ package AnyEvent::MPRPC;
 
 use strict;
 use warnings;
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 use AnyEvent::MPRPC::Server;
 use AnyEvent::MPRPC::Client;
 use base 'Exporter';
