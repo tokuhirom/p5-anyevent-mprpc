4bddc1b70d343d2d12b6b5fcc6f113120804d8e5
Syohei YOSHIDA <syohex@gmail.com>
2012-10-17 16:46:36 +0900

    Checking in changes prior to tagging of version 0.18.
    Changelog diff is:

--- lib/AnyEvent/MPRPC.pm
+++ lib/AnyEvent/MPRPC.pm
@@ -2,7 +2,7 @@ package AnyEvent::MPRPC;
 
 use strict;
 use warnings;
-our $VERSION = '0.17';
+our $VERSION = '0.18';
 use AnyEvent::MPRPC::Server;
 use AnyEvent::MPRPC::Client;
 use base 'Exporter';
